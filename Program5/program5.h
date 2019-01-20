ebc07f37d185e77685681d4d52327f1497b073c0
Afifah Arif <cstudy07@gmail.com>
2019-01-20 01:45:49 -0600

    Create program5.h

---
+++ Program5/program5.h
@@ -0,0 +1,26 @@
+/*
+Afifah Arif
+Program 5 - The Josephus Problem (header file)
+Due Date: October 11, 2018
+*/
+
+#ifndef PROG5_H
+#define PROG5_H
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <conio.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+const int No_Items=12; // const declaration for No_Items
+const string Init_Val="A1"; // const declaration for Init_Val
+
+void init_vals(vector <string>& v, unsigned& N, unsigned& M);
+void print_vector(const vector <string>& v, const unsigned& cnt);
+
+#endif
