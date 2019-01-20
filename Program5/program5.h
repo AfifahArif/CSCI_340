b8e149bc97d4118e33a4c576efc98792a67e2c3a
Afifah Arif <cstudy07@gmail.com>
2019-01-20 03:45:36 -0600

    Update program5.h

--- Program5/program5.h
+++ Program5/program5.h
@@ -4,8 +4,8 @@ Program 5 - The Josephus Problem (header file)
 Due Date: October 11, 2018
 ***/
 
-#ifndef PROG5_H
-#define PROG5_H
+#ifndef PROGRAM5_H
+#define PROGRAM5_H
 
 #include <iostream>
 #include <iomanip>
