6fcefd689245b439521a9280ac45ee8b630f4a7f
Afifah Arif <cstudy07@gmail.com>
2019-01-20 02:27:49 -0600

    Create program1.h

---
+++ Program1/program1.h
@@ -0,0 +1,19 @@
+/***
+Programmer: Afifah Arif
+Class: CSCI 340 Section 1
+Program: 1 - Randomly Generated Numbers (header file)
+***/
+
+#ifndef PROGRAM.1_H
+#define PROGRAM.1_H
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+
+using namespace std;
+
+#endif
