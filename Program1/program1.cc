cc46370964b52e56187c3975b7c45621f1792e43
Afifah Arif <cstudy07@gmail.com>
2019-01-20 02:25:08 -0600

    Create program1.cc

---
+++ Program1/program1.cc
@@ -0,0 +1,37 @@
+/***
+Programmer: Afifah Arif
+Class: CSCI 340 - Section 1
+Program: 1 - Randomly Generated Numbers
+***/
+
+#include <iostream>		/* cout, endl */
+#include <iomanip>		/* setw  - set field width*/
+#include <vector>		/* vector */
+#include <cstdlib>		/* srand, rand */
+#include <ctime>		/* time */
+#include <algorithm>		/* sort algorithm */
+
+void genRndNums(std::vector<int>* nums)
+{
+for(int i = 0; i < 250 ; i++)
+nums->push_back(1 + rand()% 10000);
+}
+
+int main()
+{
+	srand(time(NULL));
+
+	std::vector<int>* nums = new
+		std::vector<int>();
+
+	genRndNums(nums);
+
+	std::sort(nums->begin(),
+		nums->end());
+
+	for(std::vector<int>::iterator it = nums->begin(); it != nums->end(); ++it)
+
+		std::cout << *it << std::setw(5) << " ";
+
+	return 0;
+}
