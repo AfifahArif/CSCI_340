90daac5d89717602eea5dfe9bf93e365e697f4c5
Afifah Arif <cstudy07@gmail.com>
2019-01-20 02:43:07 -0600

    Create program3.h

---
+++ Program3/program3.h
@@ -0,0 +1,18 @@
+/***
+Programmer: Afifah Arif
+Class: CSCI 340 Section 1
+Program: 3 - Sieve of Eratosthenes (header file)
+***/
+
+#ifndef PROGRAM.3_H
+#define PROGRAM.3_H
+
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <cstring>
+#include <set>
+
+using namespace std;
+
+#endif
